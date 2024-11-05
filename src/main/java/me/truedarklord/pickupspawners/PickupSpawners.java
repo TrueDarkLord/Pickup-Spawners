3a5cc318a28aeb50745436f4e89d48f3895d6a88
TrueDarkLord <truedarklord.email@gmail.com>
2024-11-05 14:06:42 -0700

    Metrics (#2)
    * Metrics
    Add bStats.
    
    * Update Gradle

--- src/main/java/me/truedarklord/pickupspawners/PickupSpawners.java
+++ src/main/java/me/truedarklord/pickupspawners/PickupSpawners.java
@@ -2,12 +2,15 @@ package me.truedarklord.pickupspawners;
 
 import me.truedarklord.pickupspawners.events.BlockBreak;
 import me.truedarklord.pickupspawners.events.BlockPlace;
+import me.truedarklord.pickupspawners.metrics.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PickupSpawners extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Metrics metrics = new Metrics(this, 23819);
+
         new BlockBreak(this);
         new BlockPlace(this);
     }
